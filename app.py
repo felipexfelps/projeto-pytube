6326a6aae0bf43b36ecb73c72289b48fa7eae14c
felipexfelps <felipeleonigalafassi@gmail.com>
2023-10-18 15:22:11 -0300

    alteracao louca

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("Hello world")
+def caracter(frase) -> str:
+    letras = 0
+    espaco = 0
+    digito = 0
+    for i in frase:
+        if i.isalpha():
+            letras += 1
+        elif i == " ":
+            espaco += 1
+        elif i.isdigit():
+            digito += 1
+    print(f"Sua frase tem {len(frase)} caracteres, tem {letras} letras, tem {digito} digitos e tem {espaco} espaços vazios")
+f = input("Digite uma frase:\n")
+caracter(f)
