78f84fa9ae46c0314f77c4a6cbb53e756b54434b
felipexfelps <felipeleonigalafassi@gmail.com>
2023-10-18 15:58:42 -0300

    alteração legal

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-def caracter(frase) -> str:
-    letras = 0
-    espaco = 0
-    digito = 0
-    for i in frase:
-        if i.isalpha():
-            letras += 1
-        elif i == " ":
-            espaco += 1
-        elif i.isdigit():
-            digito += 1
-    print(f"Sua frase tem {len(frase)} caracteres, tem {letras} letras, tem {digito} digitos e tem {espaco} espaços vazios")
-f = input("Digite uma frase:\n")
-caracter(f)
+titulo = "Cadastro"
+print(len(titulo) * "=")
+print(titulo)
+print(len(titulo) * "=")
+i = 1
+while True:
+    try:
+        quantidade = int(input("Digite a quantidade de cliente que você deseja fazer cadastro: "))
+        break     
+    except ValueError as e:
+        print("Digite apenas valores numericos", e)
+        continue
+while i <= quantidade:
+    nome = input(f"Digite o nome do {i}º cliente: ")
+    while True:
+        try:
+            idade = int(input(f"Digite a idade do {i}º cliente: "))
+            break
+        except ValueError:
+            print("Digite apenas valores numericos")
+            continue
+    email = input(f"Digite o email do {i}º cliente: ")
+    print(f"Nome: {nome.capitalize()}\nIdade: {idade}\nEmail: {email}")
+    i+=1
